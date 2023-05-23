762703c0a5fe7c6c1be9a48322d3064124017349
vedanttapiavala <vtapiavala2005@gmail.com>
2023-05-23 10:49:21 -0400

    Added a TODO to SimpleReinforcement

--- frequencyGameTheory/src/SimpleReinforcementPlayer.java
+++ frequencyGameTheory/src/SimpleReinforcementPlayer.java
@@ -44,6 +44,7 @@ public class SimpleReinforcementPlayer extends Player {
 
     @Override
     protected void update(double recentPayoff) {
+        //TODO: Make it so that probabilites cannot be negative; if probability is negative, set it to 0
         probabilities[currNoteInd]+=recentPayoff;
     }
 }
